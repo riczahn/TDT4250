92095ed85018c14b78f7d540a22e2f3b156302cc
Richard Zahn <richarza@stud.ntnu.no>
2022-11-20 19:56:12 +0100

    basic structure for TestSuiteConverter

--- no.ntnu.tdt4250.testsuite/src/transition/TestSuiteConverter.java
+++ no.ntnu.tdt4250.testsuite/src/transition/TestSuiteConverter.java
@@ -14,6 +14,14 @@ import transition.io.FileHelper;
 public class TestSuiteConverter {
 	
 	public static void main(String[] args) throws IOException {
+		processXmiFile(FileHelper.EXAMPLE_TEST_SUITE_XMI_FILE);
+		
+		// or alternatively start the workflow from a JSON file
+		// t.b.d.
+		// processJsonFile(FileHelper.EXAMPLE_TEST_SUITE_JSON_FILE);
+	}
+	
+	public static void processXmiFile(String filename) throws IOException {
 		TestSuite testSuite = FileHelper.loadTestSuite();
 		
 		// convert instance of TestSuite to instance of JavaModel
@@ -29,5 +37,4 @@ public class TestSuiteConverter {
 		
 		FileHelper.saveTestClass(testClass);
 	}
-
 }
