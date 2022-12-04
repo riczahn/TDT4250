c30a0cc0939467ac9462a663f8484654eb325331
Richard Zahn <richarza@stud.ntnu.no>
2022-12-04 20:02:18 +0100

    remove confusing line in generateJavaCode.mtl

--- no.ntnu.tdt4250.examples/java/ExampleTestSuite.java
+++ no.ntnu.tdt4250.examples/java/ExampleTestSuite.java
@@ -10,19 +10,19 @@ class ExampleTestSuite  {
 		var httpRequest = new HttpPost("https://api.ntnu.no/teachers");
 		String body = "{"name": "Leonardo Montecchi"}";
 		httpRequest.setEntity(new StringEntity(body));
-		httpRequest.setHeader("Content-Type", "application/json";
-		httpRequest.setHeader("Accept", "application/json";
+		httpRequest.setHeader("Content-Type", "application/json"
+		httpRequest.setHeader("Accept", "application/json"
 		var response = httpClient.execute(httpRequest);
 		assertThat(response.getCode()).isIn(201);
 		
-		;
+		
 		HttpClient httpClient = HttpClients.createDefault();
 		var httpRequest = new HttpGet("https://api.ntnu.no/teachers/{teacherId}");
-		httpRequest.setHeader("Accept", "application/json";
+		httpRequest.setHeader("Accept", "application/json"
 		var response = httpClient.execute(httpRequest);
 		assertThat(response.getCode()).isIn(200);
 		
-		;
+		
 	}
 	@Test
 	private void CreateandGetTeacherAgain {
@@ -30,19 +30,19 @@ class ExampleTestSuite  {
 		var httpRequest = new HttpPost("https://api.ntnu.no/teachers");
 		String body = "{"name": "Leonardo Montecchi"}";
 		httpRequest.setEntity(new StringEntity(body));
-		httpRequest.setHeader("Content-Type", "application/json";
-		httpRequest.setHeader("Accept", "application/json";
+		httpRequest.setHeader("Content-Type", "application/json"
+		httpRequest.setHeader("Accept", "application/json"
 		var response = httpClient.execute(httpRequest);
 		assertThat(response.getCode()).isIn(201);
 		
-		;
+		
 		HttpClient httpClient = HttpClients.createDefault();
 		var httpRequest = new HttpGet("https://api.ntnu.no/teachers/{teacherId}");
-		httpRequest.setHeader("Accept", "application/json";
+		httpRequest.setHeader("Accept", "application/json"
 		var response = httpClient.execute(httpRequest);
 		assertThat(response.getCode()).isIn(200);
 		
-		;
+		
 	}
 	
 }
