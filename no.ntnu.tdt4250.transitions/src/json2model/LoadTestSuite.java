be868406cedffc492f6be0de6c9c613b5eea3c97
Davide Aimar <davideai@stud.ntnu.no>
2022-12-09 19:13:00 +0100

    added json deserialization. not working

---
+++ no.ntnu.tdt4250.transitions/src/json2model/LoadTestSuite.java
@@ -0,0 +1,107 @@
+package json2model;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import testsuite.APIRequest;
+import testsuite.Header;
+import testsuite.InferrableParameter;
+import testsuite.Method;
+import testsuite.ParameterLocation;
+import testsuite.StatusAssertion;
+import testsuite.TestCase;
+import testsuite.TestSuite;
+import testsuite.TestsuiteFactory;
+
+
+public class LoadTestSuite {
+
+	public static void main(String[] args) throws IOException {
+		if (args.length != 2) {
+			System.err.println("Run with <source_file> <dest_file>");
+			return;
+		}
+		
+		TestSuite testSuite = deserializeJSON(args[0]);
+		
+		Resource testSuiteResource = new XMIResourceFactoryImpl().createResource(URI.createURI(args[1]));
+		testSuiteResource.getContents().add(testSuite);
+		testSuiteResource.save(null);
+			
+	}
+	
+	public static TestSuite deserializeJSON(String path) throws IOException {
+		File file = new File(path);  
+		
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		JsonNode node =  objectMapper.readTree(file);
+		
+		TestSuite res = TestsuiteFactory.eINSTANCE.createTestSuite();
+		res.setName(file.getName());
+		
+		TestCase tCase = TestsuiteFactory.eINSTANCE.createTestCase();
+		res.getTestCases().add(tCase);
+		tCase.setName("Test case 1"); // TODO: define this dynamically
+		
+		int id = 0;
+		
+		for (JsonNode step : node.get("testSteps") ) {
+			APIRequest tStep = TestsuiteFactory.eINSTANCE.createAPIRequest();
+			tCase.getTestSteps().add(tStep);
+			tStep.setId(id++);
+			tStep.setMethod(Method.valueOf(step.get("method").asText().toUpperCase()));
+			tStep.setUri(step.get("uri").asText());
+			
+			if (step.get("body") != null)
+				tStep.setBody(step.get("body").asText());
+			
+			if (step.get("headers") != null) {
+				step.get("headers").fields().forEachRemaining( header -> {
+					Header tHeader = TestsuiteFactory.eINSTANCE.createHeader();
+					tStep.getHeaders().add(tHeader);
+					tHeader.setKey(header.getKey());
+					tHeader.setValue(header.getValue().asText());
+				});
+			}
+			
+			if (step.get("assertions") != null) {
+				step.get("assertions").fields().forEachRemaining( assertion -> {
+					switch (assertion.getKey()) {
+					case "successfulCodes":
+						StatusAssertion sAss = TestsuiteFactory.eINSTANCE.createStatusAssertion();
+						tStep.getAssertions().add(sAss);
+						assertion.getValue().forEach(code -> {
+							sAss.getSuccessfulCodes().add(code.asInt());
+						});
+						break;
+					default:
+						throw new IllegalArgumentException("Unknown assertion type.");
+					}
+				});
+			}
+			
+			if (step.get("inferrableParameters") != null) {
+				step.get("inferrableParameters").forEach(par -> {
+					InferrableParameter iPar = TestsuiteFactory.eINSTANCE.createInferrableParameter();
+					tStep.getParameters().add(iPar);
+					iPar.setJsonPath(par.get("jsonPath").asText());
+					iPar.setName(par.get("name").asText());
+					iPar.setTestStepId(par.get("testStepId").asLong());
+					iPar.setParameterLocation(ParameterLocation.valueOf(par.get("parameterLocation").asText().toUpperCase()));
+				});
+			}
+		}
+		
+		return res;
+	}
+
+}
+
